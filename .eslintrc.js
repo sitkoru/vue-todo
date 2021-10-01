7e8f5ae8c6b3e5f30fa29b992c94f27426408da4
IgorAlymov <igor.alymov.97@mail.ru>
2021-10-01 16:36:39 +0500

    switching to vuetify

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,18 @@
-// https://eslint.org/docs/user-guide/configuring
-
 module.exports = {
   root: true,
-  parser: 'vue-eslint-parser',
-  parserOptions: {
-    parser: 'babel-eslint',
-    ecmaVersion: 2020,
-    sourceType: 'module'
-  },
   env: {
-    browser: true,
-    node: true,
-    es6: true,
-    jest: true
+    node: true
   },
-  plugins: ['vue'],
-  extends: ['plugin:vue/vue3-recommended', 'eslint:recommended'],
+  'extends': [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/typescript/recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 2020
+  },
   rules: {
-    // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'vue/html-indent': 'off'
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   }
-};
+}
--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,5 @@
 module.exports = {
-  presets: [['@babel/preset-env', { modules: false }]],
-  plugins: ['@babel/plugin-transform-runtime'],
-  env: {
-    test: {
-      presets: ['@babel/preset-env']
-    }
-  }
-};
+  presets: [
+    '@vue/cli-plugin-babel/preset'
+  ]
+}
